34bc736bdecebb4e1c5f7fdf19c20cfefa0440c6
santipu_ <santipujolsau@gmail.com>
2022-02-26 17:51:51 +0100

    Exercise solved

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
+//explain all in the comments motherfucker
 
+const leapYears = function(num) {
+    
+    //if num 
+    if (num % 100 == 0 && num % 400 == 0){
+        return true;
+    } else if (num % 100 == 0){
+        return false;
+    } else if (num % 4 == 0){
+        return true;
+    }
+    return false;
+
+
+    // this is the solution offered that is much cleaner than mine
+    // return num % 4 === 0 && ( num % 100 !== 0 || num % 400 === 0);
 };
 
 // Do not edit below this line
