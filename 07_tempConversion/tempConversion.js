599055796b18cf9302531b018a3e9e8f754c1e38
santipu_ <santipujolsau@gmail.com>
2022-02-26 18:18:12 +0100

    Exercise solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
+const ftoc = function(num) {
+  const celsius =  (num -= 32) * (5/9);
+  const rounded = Math.round(celsius * 10) / 10
+  return rounded;
 
+  // or 
+  // return Math.round((num - 32) * (5/9) * 10) / 10
 };
 
-const ctof = function() {
+const ctof = function(num) {
+  const far = num * (9/5) + 32;
+  const rounded = Math.round(far * 10) / 10
+  return rounded;
 
+  //or 
+  // return Math.round(((num * 9/5) + 32) * 10) / 10
 };
 
 // Do not edit below this line
