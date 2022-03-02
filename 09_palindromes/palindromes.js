c8fd1cdde6d77738714f58ffa9517e6f683ba1b3
santipu_ <santipujolsau@gmail.com>
2022-03-02 14:07:43 +0100

    Exercise solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const reverse = string.split("");
+    const filteredArray = reverse.filter(item => item.toLowerCase() != item.toUpperCase());
+    const original = filteredArray.join("").toLowerCase();
+    const result = filteredArray.reverse().join("").toLowerCase();
 
+    return (original === result);
 };
 
 // Do not edit below this line
