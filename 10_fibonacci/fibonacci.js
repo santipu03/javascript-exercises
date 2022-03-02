aebf5fddeee586566ebeaaa19085d73c6d5ab361
santipu_ <santipujolsau@gmail.com>
2022-03-02 14:58:57 +0100

    Exercise solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+
+const fibonacci = function(num) {
+    //If the number is negative, return error
+    if (num < 0){
+        return "OOPS";
+    } 
+    
+    //In case that the argument is a string, we convert it into a number
+    num = parseInt(num);
+
+    //We create an array of the initial sequence of Fibonacci
+    const result = [1,1];
+
+    //Loop for push to the array the sum of the 2 precedent numbers
+    for (let i = 1; i <= num-2; i++){
+        result.push(result[i] + result[i - 1]);
+    };
+
+    // return the number of the fibonacci sequence that the argument wants
+    return result[num - 1];
 
 };
 
