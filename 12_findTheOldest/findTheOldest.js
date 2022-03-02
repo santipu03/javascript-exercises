8b6c2614ea0f48f27075e6add603b6859514b693
santipu_ <santipujolsau@gmail.com>
2022-03-02 23:56:14 +0100

    Exercise solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    // If the yearOfDeath is not set (still living), set the actual year
+    for(let i = 0; i < people.length; i++){
+        if (people[i].yearOfDeath === undefined){
+            people[i].yearOfDeath = new Date().getFullYear();
+        }
+    };
+
+    // Check the years lived of everyone and keep the age of the oldest
+    const age = people.reduce((oldest, next) => {
+        const actualDate = next.yearOfDeath - next.yearOfBirth;
+        return (oldest > actualDate ? oldest : actualDate);
+    },0);
+
+    // Find the person that has lived that age and return it
+    return result = people.find(person => person.yearOfDeath - person.yearOfBirth === age);
 };
 
 // Do not edit below this line
